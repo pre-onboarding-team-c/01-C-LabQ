912ffaa0c8228eead4967c80042a6e2319249e45
scvgood287 <scvgood0763@gmail.com>
2022-06-30 21:42:57 +0900

    refactor, feat : 하수관로 갯수 목록 및 getIndices option 추가

--- app/utils/index.js
+++ app/utils/index.js
@@ -14,14 +14,43 @@ const createCommonOpenApiUrl = service => `${COMMON_OPENAPI_URL}/${service}`;
  * @param {number} limit 페이지를 의미합니다.
  * @returns {Array<number>} [startIndex, endIndex] 형식의 배열을 반환합니다.
  */
-const getIndices = (limit = 1) => {
-  const startIndex = ((limit || 1) - 1) * 10 + 1;
-  const endIndex = startIndex + 9;
+const getIndices = (limit = 1, option = 1) => {
+  const startIndex = ((limit || 1) - 1) * 10 * option + 1;
+  const endIndex = startIndex + 10 * option - 1;
 
   return [startIndex, endIndex];
+};
+
+const drainpipeAmounts = {
+  '01': 4,
+  '02': 5,
+  '03': 9,
+  '04': 11,
+  '05': 12,
+  '06': 14,
+  '07': 8,
+  '08': 5,
+  '09': 2,
+  10: 9,
+  11: 11,
+  12: 8,
+  13: 11,
+  14: 20,
+  15: 12,
+  16: 14,
+  17: 13,
+  18: 8,
+  19: 12,
+  20: 26,
+  21: 7,
+  22: 14,
+  23: 21,
+  24: 2,
+  25: 6,
 };
 
 module.exports = {
   createCommonOpenApiUrl,
   getIndices,
+  drainpipeAmounts,
 };
