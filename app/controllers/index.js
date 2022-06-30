1fe8045e98232679321198f001571aead5202d3c
scvgood287 <scvgood0763@gmail.com>
2022-06-30 21:52:15 +0900

    refactor : drainpipe 분리에 맞게 세팅

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,9 @@
 const rainfallController = require('./rainfall.controller');
+const combinationController = require('./combination.controller');
+const drainpipeController = require('./drainpipe.controller');
 
 module.exports = {
   rainfallController,
+  combinationController,
+  drainpipeController,
 };
--- app/services/index.js
+++ app/services/index.js
@@ -1,5 +1,9 @@
 const rainfallService = require('./rainfall.service');
+const combinationService = require('./combination.service');
+const drainpipeService = require('./drainpipe.service');
 
 module.exports = {
   rainfallService,
+  combinationService,
+  drainpipeService,
 };
