af86bcb04ba11d2591325b50333117ded566c9e4
scvgood287 <scvgood0763@gmail.com>
2022-06-30 20:18:55 +0900

    style : 철자, 파일, 폴더 명명 통일

--- app/routes/rainfall.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { rainFallController } = require('../controllers');
-
-router.get('/', rainFallController.getRainFall);
-
-module.exports = router;
